e58aacd723ef61f2c9a58276f9a79c52e41f150c
Xabilahu <xabilahu99@hotmail.com>
2018-10-31 10:11:53 +0100

    Fixed RunTimeError on UVA

--- src/ParenthesesBalance_673/Main.java
+++ src/ParenthesesBalance_673/Main.java
@@ -1,44 +1,50 @@
 package ParenthesesBalance_673;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.EmptyStackException;
-import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
 
-    static Scanner in;
+    static BufferedReader in;
 
     private void run() {
-        int nTest = in.nextInt();
-        for (int i = 0; i < nTest; i++) {
-            String line = in.next();
-            Stack<Character> stk = new Stack<>();
-            boolean mismatch = false;
-            int j = 0;
-            try {
-                while (j < line.length() && !mismatch) {
-                    Character actualChar = line.charAt(j++);
-                    switch (actualChar) {
-                        case '(':
-                        case '[':
-                            stk.push(actualChar);
-                            break;
-                        case ')':
-                            if (!stk.pop().equals('(')) mismatch = true;
-                            break;
-                        default:
-                            if (!stk.pop().equals('[')) mismatch = true;
-                            break;
+        try {
+            int nTest = Integer.parseInt(in.readLine());
+            for (int i = 0; i < nTest; i++) {
+                String line = in.readLine();
+                Stack<Character> stk = new Stack<>();
+                boolean mismatch = false;
+                int j = 0;
+                try {
+                    while (j < line.length() && !mismatch) {
+                        Character actualChar = line.charAt(j++);
+                        switch (actualChar) {
+                            case '(':
+                            case '[':
+                                stk.push(actualChar);
+                                break;
+                            case ')':
+                                if (!stk.pop().equals('(')) mismatch = true;
+                                break;
+                            default:
+                                if (!stk.pop().equals('[')) mismatch = true;
+                                break;
+                        }
                     }
+                    if (!mismatch && stk.empty()) System.out.println("Yes");
+                    else System.out.println("No");
+                } catch (EmptyStackException e) {
+                    System.out.println("No");
                 }
-                if (!mismatch && stk.empty()) System.out.println("Yes");
-                else System.out.println("No");
-            } catch (EmptyStackException e) {System.out.println("No");}
-        }
+            }
+        } catch (IOException e) {}
     }
 
     public static void main(String[] args) throws Exception {
-        in = new Scanner(System.in);
+        in = new BufferedReader(new InputStreamReader(System.in));
         new Main().run();
         in.close();
     }
